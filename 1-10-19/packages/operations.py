ed0d7539f0635dde43a573611c2d58214cde68d3
mouni-41 <mounikagangana06@gmail.com>
2019-10-01 17:49:23 +0530

    modules

---
+++ 1-10-19/packages/operations.py
@@ -0,0 +1,31 @@
+## Creating packages
+def add(a,b):
+    
+    c=a+b
+    print("add",c)
+add(4,6)    
+    
+def prime(m):
+    c=0
+    for i in range(1,m+1):
+        if m%i==0:
+            c=c+1
+    if(c==2):
+        print("true")
+    else:
+        print("false")
+
+def prime(m):
+    c=0
+    for i in range(1,m+1):
+        if m%i==0:
+            c=c+1
+    if(c==2):
+        print("true")
+    else:
+        print("false")
+
+                        
+            
+                
+    
